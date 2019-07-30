8198dab91d40baa8d896664610e0645bdc731fe7
unknown <you@example.com>
2019-07-30 12:25:40 +0100

    my ass

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+  <sidebar></sidebar>
+  </div>
+</template>
+
+<script>
+import sidebar from './components/sidebar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    sidebar,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
